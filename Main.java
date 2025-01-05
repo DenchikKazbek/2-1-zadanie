cc25b8af5c0e6f2cb643f12b1a6ee88b76e15eab
DenchikKazbek <sirtul@yandex.ru>
2025-01-06 11:46:59 +1200

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,10 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       int a=50;
-       double b=105.4;
-       char g='d';
-       double q=a+b+g;
-        System.out.println (q);
+        int a=5;
+        int b=12;
+        System.out.println("a==b = " + (a == b));
+        System.out.println("a!=b = " + (a != b));
+        System.out.println("a>b = " + (a > b));
+        System.out.println("a<b = " + (a < b));
+        System.out.println("a>=b = " + (a >= b));
+        System.out.println("a<=b = " + (a <=b));
     }
 }
