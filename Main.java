9377d9fdc548984a717159b0e95d1a84e1ace359
DenchikKazbek <sirtul@yandex.ru>
2025-01-05 17:05:14 +1200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Денис Соловьёв");
+       int a = 50;
+       double b = 105.4;
+       char g='d';
+       double q=a+b+g;
+        System.out.println (q);
     }
 }
