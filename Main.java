60352ef04bc22dfb8b39b937bee3232409bb196d
DenchikKazbek <sirtul@yandex.ru>
2025-01-05 22:00:20 +1200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       int a = 50;
-       double b = 105.4;
+       int a=50;
+       double b=105.4;
        char g='d';
        double q=a+b+g;
         System.out.println (q);
